6d3add2021d74d1952808974b3d94fc2cf002d3d
Damian Jaszewski <damianjaszewski@gmail.com>
2021-01-08 15:00:42 +0100

    fix input and button

--- app/src/main/java/com/example/fridgewithoutnav/MainActivity.java
+++ app/src/main/java/com/example/fridgewithoutnav/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.fridgewithoutnav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
