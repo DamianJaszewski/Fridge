9a767ad3a56ca4a2b815b9c804021f277c066bc0
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2021-01-08 20:06:02 +0100

    Add DataBase Class

---
+++ app/src/main/java/com/example/fridgewithoutnav/DataBase.java
@@ -0,0 +1,25 @@
+package com.example.fridgewithoutnav;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBase extends SQLiteOpenHelper {
+
+    public DataBase(@Nullable Context context) {
+        super(context, "ingredient.db", null, 1);
+    }
+
+    //this is called the first time a database is accessed. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
