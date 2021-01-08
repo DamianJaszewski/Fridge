b824ba7072498bb2bbd6ea44a3407a6693455fbb
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2021-01-08 23:36:51 +0100

    Add View All feature

--- app/src/main/java/com/example/fridgewithoutnav/DataBase.java
+++ app/src/main/java/com/example/fridgewithoutnav/DataBase.java
@@ -1,12 +1,23 @@
 package com.example.fridgewithoutnav;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.ContentView;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBase extends SQLiteOpenHelper {
+
+    public static final String INGREDIENT_TABLE = "INGREDIENT_TABLE";
+    public static final String COLUMN_INGREDIENT_NAME = "INGREDIENT_NAME";
+    public static final String COLUMN_INGREDIENT_DATE = "INGREDIENT_DATE";
+    public static final String COLUMN_ID = "ID";
 
     public DataBase(@Nullable Context context) {
         super(context, "ingredient.db", null, 1);
@@ -14,12 +25,68 @@ public class DataBase extends SQLiteOpenHelper {
 
     //this is called the first time a database is accessed. There should be code in here to create a new database.
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + INGREDIENT_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_INGREDIENT_NAME + " TEXT, " + COLUMN_INGREDIENT_DATE + " INT)";
+        db.execSQL(createTableStatement);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
+    }
+
+    public boolean addOne(IngredientModel ingredientModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_INGREDIENT_NAME, ingredientModel.getName());
+        cv.put(COLUMN_INGREDIENT_DATE, ingredientModel.getDate());
+
+        long insert = db.insert(INGREDIENT_TABLE, null, cv);
+        if (insert == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public boolean deleteOne(IngredientModel ingredientModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM" + INGREDIENT_TABLE + " WHERE " + COLUMN_ID + " = " + ingredientModel.getId();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+        if(cursor.moveToFirst()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public List<IngredientModel> getAll(){
+        List<IngredientModel> returnList = new ArrayList<>();
+        String queryString = "SELECT * FROM " + INGREDIENT_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+        if(cursor.moveToFirst()){
+            do {
+                int ingredientId = cursor.getInt(0);
+                String ingredientName = cursor.getString(1);
+                int ingredientDate = cursor.getInt(2);
+
+                IngredientModel newIngredient = new IngredientModel(ingredientId, ingredientName, ingredientDate);
+                returnList.add(newIngredient);
+
+            } while(cursor.moveToNext());
+        }
+        else{
+
+        }
+        cursor.close();
+        db.close();
+        return returnList;
     }
 }
--- app/src/main/java/com/example/fridgewithoutnav/MainActivity.java
+++ app/src/main/java/com/example/fridgewithoutnav/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.sql.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_add;
+    Button btn_add,btn_viewAll;
     EditText input_ingredient, input_date;
     ListView lv;
+    ArrayAdapter ingredientArrayAdapter;
+    DataBase dataBase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +29,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
         input_ingredient = findViewById(R.id.input_ingredient);
         input_date = findViewById(R.id.input_date);
         lv = findViewById(R.id.lv);
+
+        dataBase = new DataBase(MainActivity.this);
+
+        ShowIngredientOnListView(dataBase);
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                IngredientModel ingredientModel;
+
                 try{
-                    IngredientModel ingredientModel = new IngredientModel(-1, input_ingredient.getText().toString(), Integer.parseInt(input_date.getText().toString()));
+                    ingredientModel = new IngredientModel(-1, input_ingredient.getText().toString(), Integer.parseInt(input_date.getText().toString()));
                     Toast.makeText(MainActivity.this, ingredientModel.toString(), Toast.LENGTH_SHORT).show();
                 }
                 catch(Exception e){
                     Toast.makeText(MainActivity.this, "Error adding ingredient", Toast.LENGTH_SHORT).show();
+                    ingredientModel = new IngredientModel(-1, "error", 0);
                 }
+
+                DataBase dataBase = new DataBase(MainActivity.this);
+
+                boolean success = dataBase.addOne(ingredientModel);
+
+                Toast.makeText(MainActivity.this, "Success " + success, Toast.LENGTH_SHORT).show();
+                ShowIngredientOnListView(dataBase);
             }
         });
 
+        btn_viewAll.setOnClickListener(view -> {
+
+                DataBase dataBase = new DataBase(MainActivity.this);
+
+            ShowIngredientOnListView(dataBase);
+            //Toast.makeText(MainActivity.this, all.toString() , Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+
+    private void ShowIngredientOnListView(DataBase dataBase2) {
+        ingredientArrayAdapter = new ArrayAdapter<IngredientModel>(MainActivity.this, android.R.layout.simple_list_item_1, dataBase2.getAll());
+        lv.setAdapter(ingredientArrayAdapter);
     }
 }
